cce87dea8588a3b03f8a1d72bc1c66df1b704cd0
Rahma-silliman <119931759+Rahma-silliman@users.noreply.github.com>
2023-02-15 20:25:57 +0100

    Update and rename showWord.js to showWords.js

--- showWords.js
+++ showWords.js
@@ -1,5 +1,5 @@
 // Écrire une fonction qui crée un tableau mots contenant les mots "Niger", "Niamey", "Sadore" et "Codeloccol" et qui affiche chaque élément du tableau à l'écran en utilisant une boucle for. 
-function showWord(){
+function showWords(){
   mots = ["Niger", "Niamey", "Sadore", "codeloccol"];
   nomberElement = mots.length
   for(let i = 0; i < nomberElement; i++){
@@ -7,5 +7,5 @@ function showWord(){
   };
   return 
 };
-showWord();
+showWords();
 module.exports = showWords;
