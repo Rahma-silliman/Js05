17e3e3e4ec56919785b71cb83ef8428443e4b24e
Rahma-silliman <119931759+Rahma-silliman@users.noreply.github.com>
2023-02-16 10:27:21 +0100

    Update displayNumber.js

--- displayNumber.js
+++ displayNumber.js
@@ -1,9 +1,9 @@
 // Écrire une fonction qui crée un tableau nombres contenant les nombres de 1 à 10 et qui affiche chaque élément du tableau à l'écran. 
-function displayNumbers(){
+function displayNumber(){
   tableau =[1,2,3,4,5,6,7,8,9,10];
    for(let i = 0; i < tableau.length; i++){
       console.log(tableau[i]);
 };
 };
-displayNumbers()
+displayNumber()
 module.exports = displayNumber
